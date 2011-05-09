ff14228482823b63274e531369cbef0c2a2a5546
robbarnsley <robbarnsley@139f9538-d329-e9b2-1414-143295f95f72>
2011-05-09 11:49:04 +0000

    Minor update:
    Changed CRPIX reference values in frodo_red_rebin.c from 0.5 to 1 for fibre axes.
    
    Changed comment of L2STATXC key from (CONTINUUM) to (CONT), as it was being truncated in the header in L2_exec.
    
    Changed the structure of frodo_red_reformat.c so that the error_codes file and additional_keys_file are put into the header simultaneously.

--- src/frodo_red_rebin.c
+++ src/frodo_red_rebin.c
@@ -598,18 +598,18 @@ int main (int argc, char *argv []) {
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "RSS_CALIBRATION", "CUNIT2", "Pixels", "Units for axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "RSS_CALIBRATION", "CRVAL2", 1, "[pixel] Value at ref. pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "RSS_CALIBRATION", "CDELT2", 1, "[pixel] Pixel scale on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
-		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "RSS_CALIBRATION", "CRPIX2", 0.5, "[pixel] Reference pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "RSS_CALIBRATION", "CRPIX2", 1, "[pixel] Reference pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CTYPE1", "a1", "Type of co-ordinate on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CUNIT1", "Pixels", "Units for axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CRVAL1", 1, "[pixel] Value at ref. pixel on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CDELT1", 1, "[pixel] Pixel scale on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
-		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CRPIX1", 0.5, "[pixel] Reference pixel on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CRPIX1", 1, "[pixel] Reference pixel on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CTYPE2", "a2", "Type of co-ordinate on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CUNIT2", "Pixels", "Units for axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CRVAL2", 1, "[pixel] Value at ref. pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CDELT2", 1, "[pixel] Pixel scale on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
-		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CRPIX2", 0.5, "[pixel] Reference pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CRPIX2", 1, "[pixel] Reference pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CTYPE3", "Wavelength", "Type of co-ordinate on axis 3", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CUNIT3", "Angstroms", "Units for axis 3", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "CUBE_CALIBRATION", "CRVAL3", bin_wavelengths[0], "[pixel] Value at ref. pixel on axis 3", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
@@ -625,7 +625,18 @@ int main (int argc, char *argv []) {
 		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "SPEC_CALIBRATION", "CUNIT2", "Pixels", "Units for axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "SPEC_CALIBRATION", "CRVAL2", 1, "[pixel] Value at ref. pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "SPEC_CALIBRATION", "CDELT2", 1, "[pixel] Pixel scale on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
-		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "SPEC_CALIBRATION", "CRPIX2", 0.5, "[pixel] Reference pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "SPEC_CALIBRATION", "CRPIX2", 1, "[pixel] Reference pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+
+		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CTYPE1", "a1", "Type of co-ordinate on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CUNIT1", "Pixels", "Units for axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CRVAL1", 1, "[pixel] Value at ref. pixel on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CDELT1", 1, "[pixel] Pixel scale on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CRPIX1", 1, "[pixel] Reference pixel on axis 1", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CTYPE2", "a2", "Type of co-ordinate on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_str(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CUNIT2", "Pixels", "Units for axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CRVAL2", 1, "[pixel] Value at ref. pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CDELT2", 1, "[pixel] Pixel scale on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
+		write_additional_key_to_file_dbl(ADDITIONAL_KEYS_FILE, "COLCUBE_CALIBRATION", "CRPIX2", 1, "[pixel] Reference pixel on axis 2", ADDITIONAL_KEYS_FILE_WRITE_ACCESS);
 
 		// ***********************************************************************
 		// Set reb_cor_cc_ext_target frame parameters
