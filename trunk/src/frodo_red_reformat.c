4b0ce5be4b6dcca2ded3f351fe78c4a985cd88b0
robbarnsley86@gmail.com <robbarnsley86@gmail.com@139f9538-d329-e9b2-1414-143295f95f72>
2015-03-31 10:06:40 +0000

    Updating.

--- trunk/src/frodo_red_reformat.c
+++ trunk/src/frodo_red_reformat.c
@@ -260,7 +260,7 @@ int main (int argc, char *argv []) {
 
 			}
 
-			RETURN_FLAG = 2;
+			RETURN_FLAG = 7;
 			
 		} else if (!strcmp(operation, "L1_IMAGE") || !strcmp(operation, "RSS_NONSS") || !strcmp(operation, "RSS_SS")) { 
 
